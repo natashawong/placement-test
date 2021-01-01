3e0429119335affb1b1d8805e2d18fc902e984fc
natashawong <natasha.wong10@gmail.com>
2021-01-01 23:08:07 +0800

    finally finsihed getting unique set of random questions!! thank you to that one very obscure SO post - todo: better understand promises/callbacks :(

--- server/models/questionSchema.js
+++ server/models/questionSchema.js
@@ -29,4 +29,15 @@ const questionSchema = new mongoose.Schema({
 })
 
 const questions_db_conn = mongoose.createConnection(process.env.QUESTIONS_DB, {useNewUrlParser: true});
-module.exports = questions_db_conn.model('easy_question', mongoose.Schema(questionSchema));
+
+const easyQuestions = questions_db_conn.model('easy_question', mongoose.Schema(questionSchema))
+const mediumQuestions = questions_db_conn.model('medium_question', mongoose.Schema(questionSchema))
+const advanceQuestions = questions_db_conn.model('advance_question', mongoose.Schema(questionSchema))
+const nativeQuestions = questions_db_conn.model('native_question', mongoose.Schema(questionSchema))
+
+module.exports = {
+    easyQuestions: easyQuestions,
+    mediumQuestions: mediumQuestions,
+    advanceQuestions: advanceQuestions,
+    nativeQuestions: nativeQuestions
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const questionModel = require('../models/questionSchema');
+const easyQuestionModel = require('../models/questionSchema').easyQuestions;
+const mediumQuestionModel = require('../models/questionSchema').mediumQuestions;
+const advanceQuestionModel = require('../models/questionSchema').advanceQuestions;
+const nativeQuestionModel = require('../models/questionSchema').nativeQuestions;
+
 const studentModel = require('../models/studentSchema');
+
+/**
+ * GET QUESTIONS ROUTES
+ * 
+ */
 
 var easyQuestionsSeen = []
 var medQuestionsSeen = []
@@ -11,35 +20,22 @@ var nativeQuestionsSeen = []
 
 // TODO: error handling
 
-
-// TODO: fix this to do what I want to do then I should be pretty good.
-function getOneRandom() {
-    // Get the count of all users
-    questionModel.countDocuments().exec(function (err, count) {
-
-        // Get a random entry
-        var random = Math.floor(Math.random() * count)
-    
-        // Again query all users but only fetch one offset by our random #
-        questionModel.findOne().skip(random).exec(
-        function (err, result) {
-            // Tada! random user
-            console.log("random result" + result)
-        })
-    })
+async function getRandomQuestions(model, questionsSeen) {
+    const count = await model.countDocuments();
+    const questions = await Promise.all([...new Array(3)].map(() => { // TODO: change to 10 later
+        var rand = Math.floor(Math.random() * Math.floor(count));
+        while (questionsSeen.includes(rand)) {
+            rand = Math.floor(Math.random() * Math.floor(count));
+        }
+        questionsSeen.push(rand);
+        return model.findOne().skip(rand).exec();
+    }));
+    return questions;
 }
 
 router.get('/set-questions/easy', async (req, res) => {
     try {
-        const currQs = await questionModel.find({});
-        // for (q in currQs) {
-        //     if (easyQuestionsSeen.contains(q)) {
-        //         // find replacement = db.aggregate([{$sample: {size: 1}}]);
-        //         easyQuestionsSeen.push(replacement.ID);
-        //     } // pseudocode, make sure to change
-        // }
-        // console.log(easyQuestionsSeen)
-        // res.sendStatus(200).json(easyQs)
+        const currQs = await getRandomQuestions(easyQuestionModel, easyQuestionsSeen)
         res.send(currQs)
     } catch(err) {
         res.sendStatus(500)
@@ -48,18 +44,47 @@ router.get('/set-questions/easy', async (req, res) => {
 })
 
 router.get('/set-questions/medium', (req, res) => {
-    // get questions
-    console.log(medQuestionsSeen);
+    try {
+        const currQs = await getRandomQuestions(mediumQuestionModel, medQuestionsSeen)
+        res.send(currQs)
+    } catch(err) {
+        res.sendStatus(500)
+        console.log(err)
+    }
 })
 
 router.get('/set-questions/advance', (req, res) => {
-    // get questions
-    console.log(advQuestionsSeen);
+    try {
+        const currQs = await getRandomQuestions(advanceQuestionModel, advQuestionsSeen)
+        res.send(currQs)
+    } catch(err) {
+        res.sendStatus(500)
+        console.log(err)
+    }
 })
 
 router.get('/set-questions/native', (req, res) => {
-    // get questions
-    console.log(nativeQuestionsSeen);
+    try {
+        const currQs = await getRandomQuestions(nativeQuestionModel, nativeQuestionsSeen)
+        res.send(currQs)
+    } catch(err) {
+        res.sendStatus(500)
+        console.log(err)
+    }
 })
+
+/**
+ * POST STUDENT DATA ROUTES
+ * 
+ */
+
+ router.post('/submit', (req, res) => {
+     try {
+         
+     } catch(err) {
+         res.sendStatus(500)
+         console.log(err)
+     }
+ })
 
 module.exports = router;
